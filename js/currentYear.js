ea3ae6ecae0857fcfd1f1cfcdc2f76b114d117b3
rsendziuk <sendzyuk.r@gmail.com>
2024-12-11 12:00:22 +0100

    added some scripts; reuploaded image; improved html/css

---
+++ js/currentYear.js
@@ -0,0 +1,5 @@
+// Get the current year
+      const currentYear = new Date().getFullYear();
+
+      // Set the year dynamically in the element with id 'current-year'
+      document.getElementById('current-year').textContent = currentYear;
