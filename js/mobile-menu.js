f1c13b4a94f83ed4f29bd5759316b2aa51d2bfee
rsendziuk <sendzyuk.r@gmail.com>
2024-12-22 12:10:33 +0100

    close mobile menu on menu link click

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,13 +1,19 @@
 const mobileMenu = document.querySelector('.mobile-menu');
 const menuBtnOpen = document.querySelector('.menu-btn-open');
-const menuBtnClose = document.querySelector('.menu-btn-close');
+const menuBtnCloseElements = document.querySelectorAll('.menu-btn-close');
 
 const toggleMenu = () => mobileMenu.classList.toggle('is-open');
 const disableScroll = () =>
   document.body.classList.toggle('is-scroll-disabled');
 
-menuBtnOpen.addEventListener('click', toggleMenu);
-menuBtnClose.addEventListener('click', toggleMenu);
+menuBtnOpen.addEventListener('click', () => {
+  toggleMenu();
+  disableScroll();
+});
 
-menuBtnOpen.addEventListener('click', disableScroll);
-menuBtnClose.addEventListener('click', disableScroll);
+menuBtnCloseElements.forEach(element =>
+  element.addEventListener('click', () => {
+    toggleMenu();
+    disableScroll();
+  }),
+);
